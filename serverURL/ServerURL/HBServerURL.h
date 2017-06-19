309a96640c4163bb8e7a2a47dd6aed6991aee820
billhu1996 <billhu1996@gmail.com>
2017-06-19 16:11:40 +0800

    修改接口.

--- serverURL/ServerURL/HBServerURL.h
+++ serverURL/ServerURL/HBServerURL.h
@@ -10,7 +10,17 @@
 
 @interface HBServerURL : NSObject
 
-+(NSString *)getWithURL:(NSString *)URL
-                 apikey:(NSString *)apikey;
+//直接用Bundle ID获取url, 不适合具有appExtension类应用
++ (NSString *)getWithBundleIDandURL:(NSString *)URL
+                             apikey:(NSString *)apikey;
+
+//直接用app Display Name获取, 如果名字过长建议尽量避免使用
++ (NSString *)getWithAppNameAndURL:(NSString *)URL
+                            apikey:(NSString *)apikey;
+
+//用自定义的nickname获取, 涉及多个后端地址时使用更方便
++ (NSString *)getWithNickname:(NSString *)nickname
+                          URL:(NSString *)URL
+                       apikey:(NSString *)apikey;
 
 @end
--- serverURL/ServerURL/HBServerURL.m
+++ serverURL/ServerURL/HBServerURL.m
@@ -10,24 +10,38 @@
 
 @implementation HBServerURL
 
-+(NSString *)getWithURL:(NSString *)URL
-                 apikey:(NSString *)apikey {
-    NSDictionary *infoDict = [[NSBundle mainBundle] infoDictionary];
++ (NSString *)getWithBundleIDandURL:(NSString *)URL
+                             apikey:(NSString *)apikey {
     NSString *appBundleID = [[NSBundle mainBundle] bundleIdentifier];
+    return [self getWithNickname:appBundleID URL:URL apikey:apikey];
+}
+
++ (NSString *)getWithAppNameAndURL:(NSString *)URL
+                            apikey:(NSString *)apikey {
+    NSDictionary *infoDict = [[NSBundle mainBundle] infoDictionary];
+    NSString *appName = infoDict[@"CFBundleDisplayName"];
+    return [self getWithNickname:appName URL:URL apikey:apikey];
+}
+
++ (NSString *)getWithNickname:(NSString *)nickname
+                          URL:(NSString *)URL
+                       apikey:(NSString *)apikey {
+    NSDictionary *infoDict = [[NSBundle mainBundle] infoDictionary];
     NSString *version = infoDict[@"CFBundleShortVersionString"];
     NSString *realURL = [[NSUserDefaults standardUserDefaults] objectForKey:@"HBServerURL"];
-    if (!realURL || [realURL isEqualToString:@"https://www.apple.com"]) {
-        realURL = [self getWithVersion:version appBundleID:appBundleID url:URL apikey:apikey];
+    if (!realURL || [realURL isEqualToString:@"https://www.example.com"]) {
+        realURL = [self getWithVersion:version appBundleID:nickname url:URL apikey:apikey];
         [[NSUserDefaults standardUserDefaults] setObject:realURL forKey:@"HBServerURL"];
-        
     }
+    NSLog(@"Successful get URL from caches, URL:%@", realURL);
     return realURL;
 }
 
-+(NSString *)getWithVersion:(NSString *)version
-                appBundleID:(NSString *)appBundleID
-                 url:(NSString *)url
-              apikey:(NSString *)apikey {
+//同步获取URL
++ (NSString *)getWithVersion:(NSString *)version
+                 appBundleID:(NSString *)appBundleID
+                         url:(NSString *)url
+                      apikey:(NSString *)apikey {
     NSURL *URL = [NSURL URLWithString:[[NSString stringWithFormat:@"%@?api_key=%@&filter=(version=%@) and (bundle_id=%@)", url, apikey, version, appBundleID] stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]]];
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:URL];
     [request setHTTPMethod:@"GET"];
@@ -36,7 +50,7 @@
     return [self sendSynchronousRequest:request][@"url"];
 }
 
-+(NSDictionary *)sendSynchronousRequest:(NSURLRequest *)request{
++ (NSDictionary *)sendSynchronousRequest:(NSURLRequest *)request{
     dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
     NSError *error;
     NSData __block *data;
@@ -49,13 +63,19 @@
     }] resume];
     
     dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
-
-    id object = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
-    if (error != nil || object == nil || !([object isKindOfClass:[NSDictionary class]]) || !object[@"resource"] || !object[@"resource"][0]) {
+    
+    id object;
+    if (data) {
+        NSError *error;
+        object = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+    }
+    if (error != nil || object == nil || !([object isKindOfClass:[NSDictionary class]]) || object[@"resource"] == nil || [object[@"resource"] count] < 1 || object[@"resource"][0] == nil) {
+        NSLog(@"Can't get url, Error: %@", error);
         return @{
-                 @"url": @"https://www.apple.com"
+                 @"url": @"https://www.example.com"
                  };
     }
+    NSLog(@"Ssuccessful get url, URL: %@", object[@"resource"][0][@"url"]);
     return object[@"resource"][0];
 }
 
